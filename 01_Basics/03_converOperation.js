35faa2d25c099f14904a2cdcaa8db9bf0335516e
its-utkarshmishra <133182635+its-utkarshmishra@users.noreply.github.com>
2024-02-05 11:06:07 +0000

    conversions done

---
+++ 01_Basics/03_converOperation.js
@@ -0,0 +1,39 @@
+let score = "33"
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => = 
+
+
+let isLoggedIn = "Utkarsh"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// '' => false
+// 'utk' => true
+
+// let someNumber = 33
+
+// let stringNumber =  String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+// ******************************* Operations *********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1  = "Hello"
+let str2 = " Utkarsh!"
+
+let str3 = str1 + str2
+
+console.log(str3);
