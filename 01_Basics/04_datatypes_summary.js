8ea7c26edae3a824d4411ace2b27aa66f47a507c
its-utkarshmishra <133182635+its-utkarshmishra@users.noreply.github.com>
2024-02-05 11:27:24 +0000

    datatypes finish

---
+++ 01_Basics/04_datatypes_summary.js
@@ -0,0 +1,39 @@
+// Primitive 
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 112.4
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('2323')
+const anotherID = Symbol("123")
+console.log(id === anotherID);
+
+bignumber = 23243242423443433n
+console.log(typeof(bignumber));
+
+// Refrence (Non-Primitive)
+
+// Array, Objects, Functions
+
+
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
